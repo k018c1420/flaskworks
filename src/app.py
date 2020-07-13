bcd5c19d9704e78255ba632295e118a4fe225c67
Ewokkkkk <ewok.mfk@gmail.com>
2020-07-13 13:31:54 +0900

    3.3.template(dic)

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return render_template('index.html', title="タイトル")
+    return render_template('index.html', obj={"title" : "タイトル２"})
 
 if __name__ == '__main__':
     app.debug = True
