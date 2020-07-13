8418ea0995d5072334f1e602b801add8bf69508d
Ewokkkkk <ewok.mfk@gmail.com>
2020-07-13 13:45:54 +0900

    Kadai3-1

---
+++ src/Kadai3-1.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+    return render_template('index.html', num = 10)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
