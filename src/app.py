10887fdcf6e481b7ec476476201882e372341943
Ewokkkkk <ewok.mfk@gmail.com>
2020-07-13 14:17:12 +0900

    4.4.routing(path)

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', message="indexページ")
+data = ["AAA", "BBB", "CCC", "DDD"]
 
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html', message="Helloページ")
+@app.route('/post/<int:post_id>/')
+def profile(post_id):
+    if post_id < len(data):
+        return render_template('index.html', message=data[post_id])
+    else:
+        return "0 ~ " + str(len(data)-1) + "の値を入力してください"
+
 
 if __name__ == '__main__':
     app.debug = True
