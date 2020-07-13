8eb867d761991466e363e128cdfd46b7c78e6676
Ewokkkkk <ewok.mfk@gmail.com>
2020-07-13 14:11:47 +0900

    4.3.routing

--- src/app.py
+++ src/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html', message="indexページ")
 
-def hello_world():
-    list = {'apple':'リンゴ', 'orange':'みかん', 'lemon':'レモン'}
-    return render_template('index.html', list=list)
+@app.route('/hello/')
+def hello():
+    return render_template('hello.html', message="Helloページ")
 
 if __name__ == '__main__':
     app.debug = True
