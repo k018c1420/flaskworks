64f7cc588ef40640632c80be35706ae4f9f0cf24
Ewokkkkk <ewok.mfk@gmail.com>
2020-07-20 14:45:52 +0900

    5.1.http_method

--- src/app.py
+++ src/app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-data = ["AAA", "BBB", "CCC", "DDD"]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/post/<int:post_id>/')
-def profile(post_id):
-    if post_id < len(data):
-        return render_template('index.html', message=data[post_id])
-    else:
-        return "0 ~ " + str(len(data)-1) + "の値を入力してください"
+@app.route('/send', methods=['POST'])
+def send():
+    msg = request.form.get('msg')
+    return render_template('receive.html', message=msg)
 
-
-if __name__ == '__main__':
+if __name__ == '__main__' :
     app.debug = True
     app.run()
